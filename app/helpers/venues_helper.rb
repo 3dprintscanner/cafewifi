a2326b6dc9485fd5f9e4414f4c34cab4b246d41a
3DPrintScanner <1988anthonythomas@gmail.com>
2015-02-21 14:16:40 +0000

    added extra map functionality

--- app/helpers/venues_helper.rb
+++ app/helpers/venues_helper.rb
@@ -1,2 +1,11 @@
 module VenuesHelper
+
+	def google_map_api_key
+		return "AIzaSyAcXzaq551g08tKs5i0WFqCZQKe4t06UOY"
+	end
+
+	def google_url_endpoint
+		"http://maps.googleapis.com/maps/api/js"
+	end
+
 end
---
+++ spec/features/venues_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe "Adding Venuues"  do
+	let!(:venue) {Venue.create(name: "a new cafe", free_wifi: 1, charging: 1, website: "www.anewcafe.com", latitude: 48.8447, longitude: 2.25, created_at: Time.now)
+
+	it 'should create a new entry in the venue list' do
+		
+	end  
+end

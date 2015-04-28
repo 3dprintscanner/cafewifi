942e052363ab67a66b20070fc097f1ae1d9015e9
3DPrintScanner <1988anthonythomas@gmail.com>
2015-04-28 02:08:36 +0100

    added signin and some other functionality

--- app/models/venue_data.rb
+++ app/models/venue_data.rb
@@ -2,4 +2,6 @@ class VenueData < ActiveRecord::Base
   has_one :venue
   belongs_to :venue
 
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Cafewifi::Application.routes.draw do
+  devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  resources :users
-
   resources :reviews
 
   resources :venues
